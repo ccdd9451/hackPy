a187dbd63ea81ba6a8d9902a8c3f76fc922b83a8
Kaidong Hu <dongforregister@gmail.com>
2018-03-04 08:50:29 -0500

    basic framework

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name="hackPy",
+    version="0.1a",
+    packages=find_packages(),
+    install_requires=[],
+)
